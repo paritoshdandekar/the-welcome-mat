835f70f15fc3cb2a1ea0e8fa41118e96b4fbba64
anjali1580 <anjalirajendran931@gmail.com>
2021-04-05 22:08:37 +0530

    lunch dinner breakfast gets

--- backend/routes/menu.route.js
+++ backend/routes/menu.route.js
@@ -2,24 +2,43 @@ const router = require('express').Router();
 let Menu = require('../models/menu.model');
 
 router.route('/:id').delete((req, res) => {
-    Menu.findByIdAndDelete(req.params.id)
-      .then(() => res.json('Menu item deleted.'))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-  
-  router.route('/add').post((req, res) => {
-    const menu_name=req.body.menu_name;
-    const menu_type=req.body.menu_type;
-    const menu_price=req.body.menu_price;
-    const menu_imglink=req.body.imglink;
+  Menu.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Menu item deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 
-    const newMenu = new Menu({menu_name,menu_type,menu_price,menu_imglink});
+router.route('/breakfast').post((req, res) => {
+  menu_type.find({ menu_type: 'breakfast' })
+    .then((menus) => res.json(menus))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/lunch').post((req,res)=>{
+  menu_type.find({ menu_type: 'lunch' })
+    .then((menus) => res.json(menus))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/dinner').post((req,res)=>{
+  menu_type.find({ menu_type: 'dinner' })
+    .then((menus) => res.json(menus))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+router.route('/add').post((req, res) => {
+  const menu_name = req.body.menu_name;
+  const menu_type = req.body.menu_type;
+  const menu_price = req.body.menu_price;
+  const menu_imglink = req.body.imglink;
+
+  const newMenu = new Menu({ menu_name, menu_type, menu_price, menu_imglink });
 
   newMenu.save()
     .then(() => res.json('New Menu added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 
 
-  });
+});
 
 module.exports = router;
