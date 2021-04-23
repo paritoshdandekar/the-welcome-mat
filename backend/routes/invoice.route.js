da9f52682e3d9a80e7759c4e2fd584e1c44003c1
KunalThorane <78753419+KunalThorane@users.noreply.github.com>
2021-04-23 22:10:40 +0530

    food laundary amount

--- backend/routes/invoice.route.js
+++ backend/routes/invoice.route.js
@@ -7,20 +7,31 @@ router.route('/').get((req, res) => {
       .catch(err => res.status(400).json('Error: ' + err));
   });
   router.route('/:id').get((req, res) => {
-    Invoice.findById(req.params.id)
+    Invoice.find({userId: req.params.id})
       .then(user => res.json(user))
       .catch(err => res.status(400).json('Error: ' + err));
   });
 
-  router.route('/add').put((req, res) => {
+//   router.route('/add').put((req, res) => {
 
-    const userId=req.body.userId;
-    const amount=req.body.amount;
-    const newInvoice = new User({userId,amount});
+//     const userId=req.body.userId;
+//     const amount=req.body.amount;
+//     const newInvoice = new Invoice({userId,amount});
 
-  newInvoice.save()
-    .then(() => res.json('New User added!'))
+//   newInvoice.save()
+//     .then(() => res.json('New User added!'))
+//     .catch(err => res.status(400).json('Error: ' + err));
+// });
+router.route('/add').post((req, res) => {
+  Invoice.find({userId: req.body.userId})
+    .then(invoices => {
+
+      invoices.amount = req.body.amount;
+
+      invoices.save()
+        .then(() => res.json('Amount updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
     .catch(err => res.status(400).json('Error: ' + err));
 });
-
 module.exports = router;
